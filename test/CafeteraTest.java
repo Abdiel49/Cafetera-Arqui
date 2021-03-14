45ccaa2dc6276ae06878403a109caa4dc21baa8a
abdiel_249 <abdielorellana3@gmail.com>
2021-03-14 13:09:00 -0400

    refactor testing

--- test/CafeteraTest.java
+++ test/CafeteraTest.java
@@ -3,22 +3,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CafeteraTest {
   
-  @Test
-  public void prepareAndObtainCoffee(){
+  @Test public void prepareAndObtainCoffee(){
     Cafetera coffeemaker = new Cafetera();
     int coffee = 10, water = 350;
     coffeemaker.coffeeBrewing(water,coffee);
     assertTrue( coffeemaker.giveMeACoffee(350) );
   }
 
-  @Test
-  public void shouldNotGetACoffee(){
+  @Test public void shouldNotGetACoffee(){
     Cafetera coffeemaker = new Cafetera();
     assertFalse( coffeemaker.giveMeACoffee(250) );
   }
 
-  @Test
-  public void shouldGetThreeCoffee(){
+  @Test public void shouldGetThreeCoffee(){
     Cafetera coffeemaker = new Cafetera();
     int coffee = 24, water = 760, coffeeQuantity = 250;
     coffeemaker.coffeeBrewing(water,coffee);
@@ -28,16 +25,14 @@ class CafeteraTest {
     assertFalse( coffeemaker.giveMeACoffee(coffeeQuantity) );
   }
 
-  @Test
-  public void noCoffeeCupWithoutWater(){
+  @Test public void noCoffeeCupWithoutWater(){
     Cafetera coffeemaker = new Cafetera();
     int coffee = 18, water = 0;
     coffeemaker.coffeeBrewing(water,coffee);
     assertFalse(coffeemaker.giveMeACoffee(250));
   }
 
-  @Test
-  public void noCoffeeCupWithoutCoffee(){
+  @Test public void noCoffeeCupWithoutCoffee(){
     Cafetera coffeemaker = new Cafetera();
     int coffee = 0, water = 300;
     coffeemaker.coffeeBrewing(water,coffee);
